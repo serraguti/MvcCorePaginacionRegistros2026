be6e85216fde93eba71818f9327f7641450180ac
serraguti <pacoserranox@gmail.com>
2026-03-06 12:30:03 +0100

    Add project files.

---
+++ MvcCorePaginacionRegistros/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using MvcCorePaginacionRegistros.Data;
+using MvcCorePaginacionRegistros.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+string connectionString =
+    builder.Configuration.GetConnectionString("SqlHospital");
+builder.Services.AddTransient<RepositoryHospital>();
+builder.Services.AddDbContext<HospitalContext>
+    (options => options.UseSqlServer(connectionString));
+
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
---
+++ MvcCorePaginacionRegistros/Repositories/RepositoryHospital.cs
@@ -0,0 +1,14 @@
+﻿using MvcCorePaginacionRegistros.Data;
+
+namespace MvcCorePaginacionRegistros.Repositories
+{
+    public class RepositoryHospital
+    {
+        private HospitalContext context;
+
+        public RepositoryHospital(HospitalContext context)
+        {
+            this.context = context;
+        }
+    }
+}
